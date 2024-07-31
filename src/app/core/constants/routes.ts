4ab9bfcab51fdd99c0092e2ce2f0e9224051b3de
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-07-31 15:23:47 +0100

    remove slash from the beginning of the paths

--- src/app/core/constants/routes.ts
+++ src/app/core/constants/routes.ts
@@ -1,7 +1,7 @@
 export const ROUTES = {
-  HOME: {PATH: '/home', TITLE: 'Obs Ecommerce App | Home'},
-  ABOUT: {PATH: '/about', TITLE: 'Obs Ecommerce App | About'},
-  SHOP: {PATH: '/shop', TITLE: 'Obs Ecommerce App | Shop'},
-  CONTACT: {PATH: '/contact', TITLE: 'Obs Ecommerce App | Contact'},
-  CART: {PATH: '/cart', TITLE: 'Obs Ecommerce App | Cart'}
+  HOME: {PATH: 'home', TITLE: 'Obs Ecommerce App | Home'},
+  ABOUT: {PATH: 'about', TITLE: 'Obs Ecommerce App | About'},
+  SHOP: {PATH: 'shop', TITLE: 'Obs Ecommerce App | Shop'},
+  CONTACT: {PATH: 'contact', TITLE: 'Obs Ecommerce App | Contact'},
+  CART: {PATH: 'cart', TITLE: 'Obs Ecommerce App | Cart'}
 }
