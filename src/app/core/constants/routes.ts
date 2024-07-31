0ccadabed23e1f4e9c9a1256a322a689bc4e0258
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-07-31 15:41:51 +0100

    add LINK_TEXT

--- src/app/core/constants/routes.ts
+++ src/app/core/constants/routes.ts
@@ -1,7 +1,7 @@
 export const ROUTES = {
-  HOME: {PATH: 'home', TITLE: 'Obs Ecommerce App | Home'},
-  ABOUT: {PATH: 'about', TITLE: 'Obs Ecommerce App | About'},
-  SHOP: {PATH: 'shop', TITLE: 'Obs Ecommerce App | Shop'},
-  CONTACT: {PATH: 'contact', TITLE: 'Obs Ecommerce App | Contact'},
-  CART: {PATH: 'cart', TITLE: 'Obs Ecommerce App | Cart'}
+  HOME: {PATH: 'home', TITLE: 'Obs Ecommerce App | Home', LINK_TEXT: 'Home'},
+  ABOUT: {PATH: 'about', TITLE: 'Obs Ecommerce App | About', LINK_TEXT: 'About'},
+  SHOP: {PATH: 'shop', TITLE: 'Obs Ecommerce App | Shop', LINK_TEXT: 'Shop'},
+  CONTACT: {PATH: 'contact', TITLE: 'Obs Ecommerce App | Contact', LINK_TEXT: 'Contact'},
+  CART: {PATH: 'cart', TITLE: 'Obs Ecommerce App | Cart', LINK_TEXT: 'Cart'}
 }
