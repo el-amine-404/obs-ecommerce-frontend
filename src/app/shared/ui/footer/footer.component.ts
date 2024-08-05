b956e7e467c79770c49f4fc71dbe302341b48098
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-08-05 10:32:27 +0100

    add footer

--- src/app/shared/ui/footer/footer.component.ts
+++ src/app/shared/ui/footer/footer.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ROUTES } from '../../../core/constants/routes';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+
+      // the constants
+      public ROUTES = ROUTES;
 
 }
