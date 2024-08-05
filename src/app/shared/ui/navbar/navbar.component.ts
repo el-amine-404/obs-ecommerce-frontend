b97c24f7bcaac46233e1daed85727d1ee3e14784
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-08-05 10:19:20 +0100

    add assets needed for the template

--- src/app/shared/ui/navbar/navbar.component.ts
+++ src/app/shared/ui/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ROUTES } from '../../../core/constants/routes';
 
 @Component({
   selector: 'app-navbar',
@@ -9,5 +10,8 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+    // the constants
+    public ROUTES = ROUTES;
 
 }
