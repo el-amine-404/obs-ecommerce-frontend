12346a8be65d416c114f98c35deb85587a2ed3fa
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-08-05 15:57:55 +0100

    add product service 🚀

---
+++ src/app/core/services/api/product.service.ts
@@ -0,0 +1,25 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { APIConstant } from '../../constants/APIconstants';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../../model/product.entity';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  // constants
+  public API_CONSTANTS = APIConstant
+
+  #productUrl = `${environment.apiURL}/${this.API_CONSTANTS.PRODUCT.GET_ALL}`;
+
+  #http = inject(HttpClient);
+
+  constructor() {}
+
+  getAll(): Observable<Product[]> {
+    return this.#http.get<Product[]>(this.#productUrl);
+  }
+}
