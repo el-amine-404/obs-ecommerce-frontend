7b5e64d6f861ea783ee9db79a5ca62b87c06878e
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-08-06 09:44:54 +0100

    add shopSingle component

---
+++ src/app/pages/shop/shop-single/shop-single.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-single',
+  standalone: true,
+  imports: [],
+  templateUrl: './shop-single.component.html',
+  styleUrl: './shop-single.component.css'
+})
+export class ShopSingleComponent {
+
+}
