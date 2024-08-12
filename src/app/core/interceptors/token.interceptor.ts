8be2afab04e2e7d420227c8006b3f5073e8df193
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-08-12 14:40:16 +0100

    tokenInterceptor: add additonal checks before sending authorization header
    this helps not to send headers with 'bearer null'

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -5,10 +5,15 @@ import { inject } from '@angular/core';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const localStorageService = inject(LocalStorageService);
+  const jwtToken = localStorageService.getItem('access-token');
+
+  if (!jwtToken) {
+    return next(req);
+  }
 
   if(!req.url.includes('login')) {
     let newRequest = req.clone({
-      headers: req.headers.set('Authorization', 'Bearer ' + localStorageService.getItem('access-token'))
+      headers: req.headers.set('Authorization', `Bearer ${jwtToken}`)
     });
     return next(newRequest);
   }
