c3d52bb07ce8d37864425916eabf49ff79aae588
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-08-12 13:41:36 +0100

    add interceptor

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -1,5 +1,17 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { LocalStorageService } from '../services/storage/local-storage.service';
+import { inject } from '@angular/core';
+
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const localStorageService = inject(LocalStorageService);
+
+  if(!req.url.includes('login')) {
+    let newRequest = req.clone({
+      headers: req.headers.set('Authorization', 'Bearer ' + localStorageService.getItem('access-token'))
+    });
+    return next(newRequest);
+  }
+
   return next(req);
 };
